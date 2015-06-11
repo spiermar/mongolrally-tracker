6ccc8db8ed4b67be82643e3b75f8048903cba1f0
Martin Spier <spiermar@gmail.com>
2015-06-10 17:58:29 -0700

    Moving secret_key to config file.

--- tracker/__init__.py
+++ tracker/__init__.py
@@ -6,10 +6,13 @@ Initialize Flask app
 from flask import Flask
 import logging
 import os
+import ConfigParser
 
+config = ConfigParser.ConfigParser()
+config.read(os.getcwd() + '/app.config')
 
 app = Flask(__name__, static_folder=os.getcwd() + '/static', static_url_path='', template_folder=os.getcwd() + '/templates')
 
-app.secret_key = '6yU5561yRuhVksj8<453(-ZS69"0s3kzTAbCETvUA3^A?AJri4BTy4`drV|.2?9'
+app.secret_key = config.get('Tracker', 'secret_key')
 
 import views
