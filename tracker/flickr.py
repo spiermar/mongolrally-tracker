9d6b1dd3bc09644f8f2dda27996667c32d478fc3
Martin Spier <spiermar@gmail.com>
2015-06-20 13:31:18 -0700

    Not adding photos that were already imported.

--- tracker/flickr.py
+++ tracker/flickr.py
@@ -23,49 +23,51 @@ def import_photos(username, photoset_title):
             photos = photoset.getPhotos()
             for photo in iter(photos):
                 photo_id = int(photo.id)
-                latitude = None
-            	longitude = None
-                timestamp = None
-            	title = photo.title
-                thumb = None
-                image = None
-                photopage = None
-                info = photo.getInfo()
-                taken = info[u'taken']
-                timestamp = datetime.strptime(taken, "%Y-%m-%d %H:%M:%S")
-                urls = info[u'urls'][u'url']
-                for url in urls:
-                    if url[u'type'] == 'photopage':
-                        photopage = url[u'text']
-                        break
-                if u'location' in info:
-                    location = info[u'location']
-                    # locality = location[u'locality']
-                    # region = location[u'region']
-                    # country = location[u'country']
-                    latitude = float(location[u'latitude'])
-                    longitude = float(location[u'longitude'])
-                    # title =  "%s, %s, %s" % (locality, region, country)
-                sizes = photo.getSizes()
-                thumb = sizes[u'Square'][u'source']
-                image = sizes[u'Medium'][u'source']
+                point = Point.query(Point.pointid == photo_id).get()
+                if point is None:
+                    latitude = None
+                    longitude = None
+                    timestamp = None
+                    title = photo.title
+                    thumb = None
+                    image = None
+                    photopage = None
+                    info = photo.getInfo()
+                    taken = info[u'taken']
+                    timestamp = datetime.strptime(taken, "%Y-%m-%d %H:%M:%S")
+                    urls = info[u'urls'][u'url']
+                    for url in urls:
+                        if url[u'type'] == 'photopage':
+                            photopage = url[u'text']
+                            break
+                    if u'location' in info:
+                        location = info[u'location']
+                        # locality = location[u'locality']
+                        # region = location[u'region']
+                        # country = location[u'country']
+                        latitude = float(location[u'latitude'])
+                        longitude = float(location[u'longitude'])
+                        # title =  "%s, %s, %s" % (locality, region, country)
+                    sizes = photo.getSizes()
+                    thumb = sizes[u'Square'][u'source']
+                    image = sizes[u'Medium'][u'source']
 
-            	try:
-            	    point = Point(
-                        title=title,
-                        latitude=latitude,
-                        longitude=longitude,
-                        type="photo",
-                        timestamp=timestamp,
-                        pointid=photo_id,
-                        # thumb=thumb,
-                        # url=photopage,
-                        resource=image
-                    )
-            	    point.put()
-            	except AttributeError:
-            	    pass
-            	except Exception as e:
-            	    logging.error(e.args[0])
+                    try:
+                	    point = Point(
+                            title=title,
+                            latitude=latitude,
+                            longitude=longitude,
+                            type="photo",
+                            timestamp=timestamp,
+                            pointid=photo_id,
+                            # thumb=thumb,
+                            # url=photopage,
+                            resource=image
+                        )
+                	    point.put()
+                    except AttributeError:
+                	    pass
+                    except Exception as e:
+                	    logging.error(e.args[0])
 
     return Response(json.dumps({ 'status': 'ok' }), status=200, mimetype='application/json');
